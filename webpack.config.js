13f421850943d93770c35cb8724a2ed3a0cc10f1
Naman Bhardwaj <namanbhardwaj17@gmail.com>
2018-09-27 19:49:13 +0530

    First commit + webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+let path = require("path");
+let htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_one.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: "/node_modules/",
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
